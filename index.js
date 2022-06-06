1787bed68f4203973006f4a43eaee748291613e6
NouhRastanawi <noahrastdark@gmail.com>
2022-06-06 13:58:28 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  return word == word.split('').reverse().join('');
 }
 
+console.log("Check: ", isPalindrome("racecar"));
+
+
 /* 
-  Add your pseudocode here
+  Similarly to the previous assignemnt. We split the word and then reverse the letters and rejoin them again.
+  Then we do the check. If the orignal word is the same after reversing
+  --> the word is Palindrome (true). Otherwise it is not (false).
 */
 
-/*
-  Add written explanation of your solution here
-*/
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");cle
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
